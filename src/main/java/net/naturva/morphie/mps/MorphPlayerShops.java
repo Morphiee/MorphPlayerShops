21d1041239e39b9dc1c663dafd1829d69e5dc264
Morphiee <morphie108@gmail.com>
2020-11-18 04:23:50 -0800

    Initial plugin setup

---
+++ src/main/java/net/naturva/morphie/mps/MorphPlayerShops.java
@@ -0,0 +1,15 @@
+package net.naturva.morphie.mps;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MorphPlayerShops extends JavaPlugin {
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+
+}
