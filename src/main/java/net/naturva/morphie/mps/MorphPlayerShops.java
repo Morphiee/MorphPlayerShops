c5131916489b1820b39d3337a3c85a445f076f91
Morphiee <morphie108@gmail.com>
2020-11-19 04:47:06 -0800

    + VaultAPI: Chat, Econ, Perms + Startup information.

--- src/main/java/net/naturva/morphie/mps/MorphPlayerShops.java
+++ src/main/java/net/naturva/morphie/mps/MorphPlayerShops.java
@@ -1,8 +1,13 @@
 package net.naturva.morphie.mps;
 
 import jdk.nashorn.internal.objects.annotations.Getter;
+import net.milkbowl.vault.chat.Chat;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.permission.Permission;
 import net.naturva.morphie.mps.files.Messages;
 import net.naturva.morphie.mps.util.MessageMethods;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
@@ -12,13 +17,76 @@ public class MorphPlayerShops extends JavaPlugin {
     public Messages messagescfg;
     private String version = "2.0.0";
 
+    private static Economy econ = null;
+    private static Permission perms = null;
+    private static Chat chat = null;
+
     public void onEnable() {
+        getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&7‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾"));
+        getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&3MorphPlayerShops &8- &a" + this.version));
         loadConfigManager();
         createConfig();
+        if (!setupEconomy()) {
+            getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&3Economy&8: &cNot Found!"));
+            getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&3Permissions&8: &cNot Found!"));
+            getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&3Chat&8: &cNot Found!"));
+            getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&cPlugin disabled due to no Vault dependecy found!"));
+            getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', " "));
+            getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&7‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾"));
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+        setupPermissions();
+        getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&3PlayerFiles&8: &aLoaded."));
+        getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&3Economy&8: &aVault Found."));
+        getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&3Permissions&8: &aVault Found."));
+        getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&3Chat&8: &aVault Found."));
+        getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&3Plugin Status&8: &aEnabled!"));
+        getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', " "));
+        getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&7‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾"));
+
     }
 
     public void onDisable() {
+        getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&7‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾"));
+        getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&3MorphPlayerShops &8- &a" + this.version));
+        getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&3Economy&8: &cDisabled."));
+        getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&3Permissions&8: &cDisabled."));
+        getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&3Chat&8: &cDisabled."));
+        getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&3Plugin Status&8: &cDisabled!"));
+        getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', " "));
+        getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&7‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾"));
 
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+
+    private boolean setupPermissions() {
+        RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
+        perms = rsp.getProvider();
+        return perms != null;
+    }
+
+    public static Economy getEconomy() {
+        return econ;
+    }
+
+    public static Permission getPermissions() {
+        return perms;
+    }
+
+    public static Chat getChat() {
+        return chat;
     }
 
     private void createConfig() {
